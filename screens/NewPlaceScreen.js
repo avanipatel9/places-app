a05ac17f8e2f177ba22d45eb59e26acc7a93e766
Avani Patel <anpatel36@yahoo.in>
2021-07-08 23:38:22 +0530

    Accessing the Device Camera

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -4,6 +4,7 @@ import {useDispatch} from 'react-redux';
 
 import Colors from '../constants/Colors';
 import * as placesActions from '../store/action/places-actions';
+import ImagePicker from '../components/ImagePicker';
 
 const NewPlaceScreen = props => {
     const [titleValue, setTitleValue] = useState('');
@@ -28,6 +29,7 @@ const NewPlaceScreen = props => {
                     onChangeText={titleChangeHandler}
                     value={titleValue}
                 />
+                <ImagePicker />
                 <Button
                     title='Save Place'
                     color={Colors.primary}
