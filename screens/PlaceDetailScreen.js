36aedde82e4a22ff6ba1f8fdc797b58bd229202a
Avani Patel <anpatel36@yahoo.in>
2021-07-30 16:04:56 -0400

    Finishing the "Map" Screen

--- screens/PlaceDetailScreen.js
+++ screens/PlaceDetailScreen.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import {ScrollView, Image, View, Text, StyleSheet} from 'react-native';
-import {useSelector} from 'react-redux';
+import { ScrollView, Image, View, Text, StyleSheet } from 'react-native';
+import { useSelector } from 'react-redux';
 
 import MapPreview from '../components/MapPreview';
 import Colors from '../constants/Colors';
 
 const PlaceDetailScreen = props => {
     const placeId = props.navigation.getParam('placeId');
-    const selectedPlace = useSelector(state => 
+    const selectedPlace = useSelector(state =>
         state.places.places.find(place => place.id === placeId)
-        );
+    );
 
-    return(
-        <ScrollView contentContainerStyle={{alignItems: 'center'}} >
-            <Image source={{uri: selectedPlace.imageUri}} style={styles.image} />
+    const selectedLocation = { lat: selectedPlace.lat, lng: selectedPlace.lng };
+
+    const showMapHandler = () => {
+        props.navigation.navigate('Map', {readonly: true, initialLocation: selectedLocation});
+    }
+
+    return (
+        <ScrollView contentContainerStyle={{ alignItems: 'center' }} >
+            <Image source={{ uri: selectedPlace.imageUri }} style={styles.image} />
             <View style={styles.locationContainer} >
                 <View style={styles.addressContainer}><Text style={styles.address}>{selectedPlace.address}</Text></View>
-                <MapPreview style={styles.mapPreview} location={{lat: selectedPlace.lat, lng: selectedPlace.lng}} />
+                <MapPreview
+                    style={styles.mapPreview}
+                    location={selectedLocation}
+                    onPress={showMapHandler}
+                />
             </View>
 
         </ScrollView>
@@ -31,10 +41,10 @@ PlaceDetailScreen.navigationOptions = navData => {
 
 const styles = StyleSheet.create({
     image: {
-       height: '35%',
-       minHeight: 300,
-       width: '100%',
-       backgroundColor: '#ccc' 
+        height: '35%',
+        minHeight: 300,
+        width: '100%',
+        backgroundColor: '#ccc'
     },
     locationContainer: {
         marginVertical: 20,
